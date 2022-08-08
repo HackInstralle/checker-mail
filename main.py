0efa32b2c5488d1a75e60bd48b3748503ec5aa4a
raiso <70200554+HackInstralle@users.noreply.github.com>
2022-08-08 07:32:49 +0700

    Add files via upload
    How to run
    
    $apt install git
    $git clone https://github.com/HackInstralle/checker-mail/
    $cd checker-mail
    $pip install request
    $pip install colorama
    $python main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os.system,requests,colorama
+
+print("welcome to my tools")
+
+key =a+'str('http://apilayer.net/api/check?access_key='['a']:
+    
+    os.system('content validation')
+    if access_key:validation['a']
+    clear
+      int key['a'] print('Success')
+      if a('Isvalid'):
+          print('[*]' + str('content')')
+          open('valid.txt','a').write,'/n')
+          
+    clear()
+    
+          
